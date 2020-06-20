147ff2c80218eff104e77952888910a4b63b3259
VadikShikitov <shikitov.vadya@yandex.ru>
2020-06-20 15:23:36 +0700

    add bookmark create logic

--- src/components/CreateBookmarkComponent/index.tsx
+++ src/components/CreateBookmarkComponent/index.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
 import styles from './style.module.css';
 
-export const CreateBookmarkComponent: React.FC = () => {
+interface CreateBookmarComponentProps {
+  onChangeHandler(event: React.ChangeEvent<HTMLInputElement>): void;
+  createBookmark(event: React.MouseEvent<HTMLButtonElement>): void;
+}
+
+export const CreateBookmarkComponent: React.FC<CreateBookmarComponentProps> = ({
+  onChangeHandler,
+  createBookmark,
+}) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Добавить закладку</h1>
       <div className={styles.text_field}>
         <span className={styles.name}>Название:</span>
-        <input type='text' className={styles.filed} />
+        <input
+          type='text'
+          name='name'
+          onChange={onChangeHandler}
+          className={styles.filed}
+        />
       </div>
       <div className={styles.text_field}>
         <span className={styles.name}>Адрес (URL):</span>
-        <input type='text' className={styles.filed} />
+        <input
+          type='text'
+          name='URL'
+          onChange={onChangeHandler}
+          className={styles.filed}
+        />
       </div>
       <div className={styles.text_field}>
         <span className={styles.name}>Тэги:</span>
-        <input type='text' className={styles.filed} />
+        <input
+          type='text'
+          name='tags'
+          onChange={onChangeHandler}
+          className={styles.filed}
+        />
       </div>
       <div className={styles.buttons}>
         <button className={styles.buttons_cancel}>Отмена</button>
-        <button className={styles.buttons_add}>Добавить закладку</button>
+        <button className={styles.buttons_add} onClick={createBookmark}>
+          Добавить закладку
+        </button>
       </div>
     </div>
   );
--- src/containers/CreateBookmarkContainer/index.tsx
+++ src/containers/CreateBookmarkContainer/index.tsx
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { CreateBookmarkComponent } from '../../components/CreateBookmarkComponent';
 
-export const CreateBookmarkContainer: React.FC = () => (
-  <CreateBookmarkComponent />
-);
+interface bookmarkState {
+  name: string;
+  URL: string;
+  tags: string;
+}
+
+export const CreateBookmarkContainer: React.FC = () => {
+  const [bookmark, setBookmark] = useState<bookmarkState>({
+    name: '',
+    URL: '',
+    tags: '',
+  });
+
+  const onChangeHandler = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = event.target;
+      setBookmark(prevBookmark => ({ ...prevBookmark, [name]: value }));
+    },
+    [setBookmark, bookmark],
+  );
+
+  const createBookmark = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+      const ID = '_' + Math.random().toString(36).substr(2, 9);
+      const date = new Date();
+      localStorage.setItem('state', JSON.stringify({ ...bookmark, ID, date }));
+    },
+    [bookmark, setBookmark],
+  );
+
+  return (
+    <CreateBookmarkComponent
+      onChangeHandler={onChangeHandler}
+      createBookmark={createBookmark}
+    />
+  );
+};
